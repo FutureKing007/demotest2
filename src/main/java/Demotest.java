0ae52f5db817fb24ead71d3774662fd5e9c6ecfc
lzx <zixiongli@qq.com>
2018-12-01 11:16:17 +0800

    Merge branch 'master' of ssh://192.168.12.131/home/git/mytest
    # Conflicts:
    #	src/main/java/Demotest.javadfg

---
+++ src/main/java/Demotest.java
@@ -0,0 +1,9 @@
+public class Demotest {
+    public static void main(String[] args) {
+        System.out.println("testGit");
+        System.out.println("lalal");
+		System.out.println("111111111111");
+        System.out.println("A 程序员要下班啦");
+        System.out.println("B程序员也要下班了");
+    }
+}
